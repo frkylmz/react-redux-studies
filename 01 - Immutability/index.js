1aaa830585bb2f9493915e8b15da27c3c1039629
frkylmz <farukyilmaz420@gmail.com>
2022-08-08 21:25:17 +0300

    first commit

---
+++ 01 - Immutability/index.js
@@ -0,0 +1,27 @@
+// let name = "faruk";
+// let name2 = name;
+
+// name2 = "murat";
+
+// console.log(name);
+// console.log(name2);
+
+// const user = {
+//   name: "Faruk",
+//   isActive: true,
+// };
+
+// const user2 = user;
+// user2.name = "Murat";
+// user.isActive = false;
+
+// console.log(user);
+// console.log(user2);
+
+const numbers = [1, 2, 3, 4];
+const numbers2 = [...numbers];
+
+numbers2.push(5);
+
+console.log(numbers);
+console.log(numbers2);
