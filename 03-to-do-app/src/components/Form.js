ffc74087fa836c4bd6285b222b9e40f725f9125a
frkylmz <farukyilmaz420@gmail.com>
2022-08-10 15:40:23 +0300

    add new todo

--- 03-to-do-app/src/components/Form.js
+++ 03-to-do-app/src/components/Form.js
@@ -1,12 +1,29 @@
-import React from "react";
+import { useState } from "react";
+
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { addTodo } from "../redux/todos/todosSlice";
 
 function Form() {
+  const [title, setTitle] = useState("");
+
+  const dispacth = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispacth(addTodo({ id: nanoid, title, completed: false }));
+
+    setTitle("");
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         className="new-todo"
         placeholder="What needs to be done?"
         autoFocus
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
     </form>
   );
