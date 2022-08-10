c779aeb8bbbba5461e3128a2a65bba01be7b5a20
frkylmz <farukyilmaz420@gmail.com>
2022-08-10 15:55:28 +0300

    add delete procedure

--- 03-to-do-app/src/components/TodoList.js
+++ 03-to-do-app/src/components/TodoList.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 import { useSelector, useDispatch } from "react-redux";
-import { toggle } from "../redux/todos/todosSlice";
+import { toggle, destroy } from "../redux/todos/todosSlice";
 
 function TodoList() {
   const dispatch = useDispatch();
 
   const items = useSelector((state) => state.todos.items);
 
+  const handleDestroy = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(destroy(id));
+    }
+  };
   return (
     <ul className="todo-list">
       {items.map((item) => (
@@ -20,7 +25,10 @@ function TodoList() {
               onChange={() => dispatch(toggle({ id: item.id }))}
             />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button
+              className="destroy"
+              onClick={() => handleDestroy(item.id)}
+            ></button>
           </div>
         </li>
       ))}
