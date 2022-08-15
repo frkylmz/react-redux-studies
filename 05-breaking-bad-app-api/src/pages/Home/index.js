6e11e6063c4c5eb10382f927696b9008eb86ccb7
frkylmz <farukyilmaz420@gmail.com>
2022-08-15 13:32:55 +0300

    add character list

--- 05-breaking-bad-app-api/src/pages/Home/index.js
+++ 05-breaking-bad-app-api/src/pages/Home/index.js
@@ -1,18 +1,52 @@
 import { useEffect } from "react";
 
+import "./styles.css";
+import Masonry from "react-masonry-css";
+
+import Loading from "../../components/Loading";
+import Error from "../../components/Error";
+
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCharacters } from "../../redux/charactersSlice";
 
 function Home() {
-  const data = useSelector((state) => state.characters);
+  const characters = useSelector((state) => state.characters.items);
+  const isLoading = useSelector((state) => state.characters.isLoading);
+  const error = useSelector((state) => state.characters.error);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCharacters());
   }, [dispatch]);
 
-  console.log(data);
-  return <div>Home</div>;
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error message={error} />;
+  }
+
+  return (
+    <div>
+      <Masonry
+        breakpointCols={4}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {characters.map((character) => (
+          <div key={character.char_id}>
+            <img
+              alt={character.name}
+              src={character.img}
+              className="character"
+            />
+            <div className="char_name">{character.name}</div>
+          </div>
+        ))}
+      </Masonry>
+    </div>
+  );
 }
 
 export default Home;
--- 05-breaking-bad-app-api/src/redux/charactersSlice.js
+++ 05-breaking-bad-app-api/src/redux/charactersSlice.js
@@ -2,11 +2,13 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import axios from "axios";
 
+const char_limit = 12;
+
 export const fetchCharacters = createAsyncThunk(
   "characters/getCharacters",
   async () => {
     const res = await axios(
-      `${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=${char_limit}`
     );
     return res.data;
   }
@@ -16,11 +18,20 @@ export const charactersSlice = createSlice({
   name: "characters",
   initialState: {
     items: [],
+    isLoading: false,
   },
   reducers: {},
   extraReducers: {
+    [fetchCharacters.pending]: (state, action) => {
+      state.isLoading = true;
+    },
     [fetchCharacters.fulfilled]: (state, action) => {
-      console.log(action.payload);
+      state.items = action.payload;
+      state.isLoading = false;
+    },
+    [fetchCharacters.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
     },
   },
 });
