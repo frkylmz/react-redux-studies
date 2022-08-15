eae96bddff188cfddeb6ac27e0db819604b04111
frkylmz <farukyilmaz420@gmail.com>
2022-08-15 12:48:58 +0300

    add breaking bad api

---
+++ 05-breaking-bad-app-api/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
