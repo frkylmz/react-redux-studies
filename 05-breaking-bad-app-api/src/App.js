d46ae0ef07defd63c0b4fb690be43f346f555ed9
frkylmz <farukyilmaz420@gmail.com>
2022-08-16 13:54:18 +0300

    add quotes route

--- 05-breaking-bad-app-api/src/App.js
+++ 05-breaking-bad-app-api/src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 // pages
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Quotes from "./pages/Quotes";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Characters</Link>
+          </li>
+          <li>
+            <Link to="/quotes">Quotes</Link>
+          </li>
+        </ul>
+      </nav>
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/char/:char_id" component={Detail} />
+          <Route path="/quotes" component={Quotes} />
         </Switch>
       </div>
     </Router>
