cd43e4a12eaae69ef5b24422f11b83340db1e097
frkylmz <farukyilmaz420@gmail.com>
2022-08-19 14:00:25 +0300

    add form and style procedures

--- 06-contacts-app/src/components/Contacts/Form.js
+++ 06-contacts-app/src/components/Contacts/Form.js
@@ -6,24 +6,34 @@ import { addContact } from "../../redux/contactSlice";
 
 function Form() {
   const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!name) return false;
-    dispatch(addContact({ id: nanoid(), name }));
+    if (!name || !number) return false;
+    dispatch(addContact({ id: nanoid(), name, phone_number: number }));
     setName("");
+    setNumber("");
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input
-          placeholder="name"
+          placeholder="Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        <input
+          placeholder="Phone Number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <div className="btn">
+          <button type="submit">Add</button>
+        </div>
       </form>
     </div>
   );
